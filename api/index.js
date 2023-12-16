098c96690ad1686947b6ae0c3a705ad26a4187c7
Salami Azeez <salamitopeazeez@gmail.com>
2023-12-15 18:01:34 -0800

    Created routes folder and added routes to index file (index.js)

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import morgan from "morgan"
 
 const app = express()
 //-------------------------Env
@@ -13,14 +14,35 @@ const connect = async () => {
             uri: process.env.MONGO_TEST
         }
         await mongoose.connect(CONFIG.uri)
-        console.log(`MongoDB started with server......`)
     }catch(error) {
         throw error
     }
 }
+mongoose.connection.on("disconnected", () => {
+    console.log("Database has been disconnected")
+})
+mongoose.connection.on("connected", () => {
+    console.log(`Database is connected......`)
+})  
 
+//-------------------------Middlewares
+app.use(express.json())
+app.use(morgan("common"))
 
+//-------------------------Routes
+import authRoute from "./routes/auth.js"
+import userRoute from "./routes/users.js"
+import hotelRoute from "./routes/hotels.js"
+import roomRoute from "./routes/rooms.js"
 
+app.use("/api/auth", authRoute)
+app.use("/api/hotels", hotelRoute)
+app.use("/api/rooms", roomRoute)
+app.use("/api/users", userRoute)
+
+app.get("/", (req, res) => {
+    res.send("This is the homepage")
+})
 
 app.listen(8800, () => {
     connect()
