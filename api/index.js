fc5e74976cfa7970065ed302d4b3d2902123399d
Salami Azeez <salamitopeazeez@gmail.com>
2023-12-15 17:24:57 -0800

    Added database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 const app = express()
+//-------------------------Env
+dotenv.config({ path: "./config/config.env" })
+//-------------------------linking Database
+mongoose.set('strictQuery', false)
+const connect = async () => {
+    try{
+        const CONFIG = {
+            uri: process.env.MONGO_TEST
+        }
+        await mongoose.connect(CONFIG.uri)
+        console.log(`MongoDB started with server......`)
+    }catch(error) {
+        throw error
+    }
+}
+
+
+
 
 app.listen(8800, () => {
+    connect()
     console.log(`Server is running on port: 8800`)
 })
