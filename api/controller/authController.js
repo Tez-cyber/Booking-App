1e6ed52ad99a588fe22cd4aa1467eccec0a94686
Salami Azeez <salamitopeazeez@gmail.com>
2023-12-18 08:28:23 -0800

    Installed bcrypt to hash user's password and updated register api

--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,16 +1,19 @@
 import User from "../model/User.js"
+import bcrypt from "bcryptjs"
 
 
 class App {
     //--------------Register User
     registerUser = async (req, res, next) => {
         const { username, email, password } = req.body
-
+        
         try {
+            const salt = bcrypt.genSaltSync(10)
+            const hashedPassword = bcrypt.hashSync(password, salt)
             const newUser = new User({
                 username,
                 email,
-                password
+                password: hashedPassword
             })
             const saveUser = await newUser.save()
             res.status(200).json(saveUser)
