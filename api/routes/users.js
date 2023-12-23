80ca39e01b992a7e24f75e06165735963f2af4a3
Salami Azeez <salamitopeazeez@gmail.com>
2023-12-23 00:18:38 -0800

    Create verifyToken file to authenticate user and test token in user routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,11 @@
 import express from "express"
 const router = express.Router()
 import userController from "../controller/userController.js"
+import { verifyToken } from "../utils/verifyToken.js"
 
-
+router.get("/checkauthentication", verifyToken, (req, res) => {
+    res.send("Hello user, you are logged in")
+})
 router.put("/:id", userController.updateUser)
 router.delete("/:id", userController.deleteUser)
 router.get("/:id", userController.getUser)
