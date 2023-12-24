c7f6b6b725717bc1ed009ba5fa1d9222217d9189
Salami Azeez <salamitopeazeez@gmail.com>
2023-12-24 00:33:36 -0800

    Adding token to hotel routes to verify access

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,10 +2,11 @@ import express from "express"
 const router = express.Router()
 
 import hotelContoller from "../controller/hotelController.js"
+import { verifyAdmin } from "../utils/verifyToken.js"
 
-router.post("/", hotelContoller.createHotel)
-router.put("/:id", hotelContoller.updateHotel)
-router.delete("/:id", hotelContoller.deleteHotel)
+router.post("/", verifyAdmin, hotelContoller.createHotel)
+router.put("/:id", verifyAdmin, hotelContoller.updateHotel)
+router.delete("/:id", verifyAdmin, hotelContoller.deleteHotel)
 router.get("/:id", hotelContoller.getHotel)
 router.get("/", hotelContoller.getAllHotel)
 
