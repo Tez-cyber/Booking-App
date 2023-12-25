57ca2ef6a2eba595501b8dd3945525a1035b38b2
Salami Azeez <salamitopeazeez@gmail.com>
2023-12-25 13:00:45 -0800

    Created roomController with create, update, delete, get room and get all rooms api

---
+++ api/controller/roomController.js
@@ -0,0 +1,75 @@
+import Room from "../model/Room.js"
+import Hotel from "../model/Hotel.js"
+import { createError } from "../utils/error.js"
+
+class App {
+    //--------------Creating room
+    createRoom = async (req, res, next) => {
+        const hotel = req.params.hotelId
+        const newRoom = new Room(req.body)
+
+        try {
+            const savedRoom = await newRoom.save()
+            try {
+                await Hotel.findByIdAndUpdate(
+                    hotelId,
+                    {$push: { rooms: savedRoom._id }}
+                )
+            }catch(err) {
+                next(err)
+            }
+            res.status(200).json(savedRoom)
+        }catch(err) {
+            next(err)
+        }
+    }
+    //------------------Update Hotel information
+    updateRoom = async (req, res, next) => {
+
+        try{
+            const updatedRoom = await Room.findByIdAndUpdate(
+                req.params.id, 
+                {$set: req.body},
+                {new: true}
+            )
+            res.status(200).json(updatedRoom)
+        }catch(err) {
+            next(err)
+        }
+    }
+    //------------------Delete Room information
+    deleteRoom = async (req, res, next) => {
+
+        try{
+            await Room.findByIdAndDelete(
+                req.params.id
+            )
+            res.status(200).json("Room has been deleted")
+        }catch(err) {
+            next(err)
+        }
+    }
+    //------------------Get Room information
+    getRoom = async (req, res, next) => {
+
+        try{
+            const findRoom = await Room.findById(
+                req.params.id
+            )
+            res.status(200).json(findRoom)
+        }catch(err) {
+            next(err)
+        }
+    }
+    //------------------Get all Room information
+    getAllRoom = async (req, res, next) => {
+        try{
+            const allHotels = await Hotel.find()
+            res.status(200).json(allHotels)
+        }catch(err) {
+            next(err)
+        }
+    }
+}
+const newApp = new App
+export default newApp
